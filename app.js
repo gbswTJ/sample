53d5528e3f66f4971e2e4214adf8f34ce010b46d
choipaco <kfckim1014@naver.com>
2023-07-09 20:50:24 +0900

    sample

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+let corsOptions = {
+  origin: "*",
+  Credential: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.static(path.join(__dirname, 'test/build')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'test/build/index.html'));
+})
+
+module.exports = app
